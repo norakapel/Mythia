788f449a3d1eb8e29d41ebbb98c2a796f80a7cb8
Paarested <t8200015@aueb.gr>
2021-11-28 16:10:14 +0200

    Added class Hraklis.java that has all the data for this character

---
+++ Hraklis.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Hraklis {
+
+	public static void main (String[] args) {
+		
+		String [] Insert1 = new String [3];
+		String [] Questions1 = new String [10];
+		String [] Rights1 = new String [10];
+		String [] Wrongs1 = new String [10];
+		int j =0;
+		String file = "src//HRAKLIS.part1.csv";
+		BufferedReader reader = null;
+		String line = "";
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while ((line = reader.readLine()) != null) {
+				
+				int k =0;
+				String[] row = line.split(",");
+				for (String index : row ) {
+					
+					Insert1[k] =index ;
+					k++;
+				}
+
+				Questions1[j] = Insert1[0];
+				Rights1[j] = Insert1[1];
+				Wrongs1[j] = Insert1[2];
+				j++;
+			}
+		}catch(Exception e){
+			
+			e.printStackTrace();
+			
+		}finally {
+			try {
+				reader.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+							
+		}
+	}
+}
